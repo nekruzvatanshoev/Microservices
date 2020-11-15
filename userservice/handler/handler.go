1fc8308c4308d69228692a774572384313628275
Nekruz Vatanshoev <nekruz.vatanshoev@outlook.com>
2020-11-15 11:51:38 -0600

    Adding handler package to userservice (Home and Login handlers)

---
+++ userservice/handler/handler.go
@@ -0,0 +1,46 @@
+package handler
+
+import (
+	"Microservices/userservice/domain"
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+func Home(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	fmt.Fprintln(w,"Welcome to Naboft!")
+}
+
+
+func Login(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application")
+	//var db domain.User
+	db := []domain.User{
+		{
+			ID: "AAABBBCCC",
+			Email: "first.last@company.com",
+			Password: "thelonestarstate",
+			Admin: true,
+		},
+		{
+			ID: "BBBCCCDDD",
+			Email: "first.last@outlook.com",
+			Password: "rockymountainhigh",
+			Admin: false,
+		},
+	}
+
+	fmt.Println(db)
+	var user domain.User
+
+	err := json.NewDecoder(r.Body).Decode(&user)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println(user)
+
+
+
+}
--- userservice/user_service.go
+++ userservice/user_service.go
@@ -1,1 +1,17 @@
 package main
+
+import (
+	"log"
+	"net/http"
+	"Microservices/userservice/handler"
+)
+
+func main() {
+	router := http.NewServeMux()
+	router.HandleFunc("/",handler.Home)
+	router.HandleFunc("login",handler.Login)
+
+	if err := http.ListenAndServe(":8080", router); err != nil {
+		log.Fatal(err)
+	}
+}
