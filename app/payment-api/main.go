89d69073a01d1ea53cd39149e9737ef380cbb365
Nekruz Vatanshoev <nekruz.vatanshoev@outlook.com>
2021-03-26 09:40:34 -0500

    feat(payment): Adding a dummy example to Payment API for Paypal.

--- app/payment-api/main.go
+++ app/payment-api/main.go
@@ -1,2 +1,62 @@
 package main
 
+import (
+	"context"
+	"fmt"
+	"github.com/joho/godotenv"
+	"github.com/plutov/paypal/v4"
+	"log"
+	"math/rand"
+	"os"
+)
+
+
+func main(){
+	godotenv.Load(".env")
+
+	clientId := os.Getenv("PAYPAL_CLIENT_ID")
+	secretId := os.Getenv("PAYPAL_SECRET")
+
+	c, err := paypal.NewClient(clientId, secretId, paypal.APIBaseSandBox)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	c.SetLog(os.Stdout)
+
+
+
+	_, err = c.GetAccessToken(context.Background())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// dummy example
+	payout := paypal.Payout{
+		SenderBatchHeader: &paypal.SenderBatchHeader{
+			SenderBatchID: "202103241234567",
+			EmailSubject:  "You have a payout!",
+			EmailMessage:  "You have received a payout! Thanks for using our service!",
+		},
+		Items: []paypal.PayoutItem{
+			{
+				RecipientType:   "EMAIL",
+				RecipientWallet: paypal.VenmoRecipientWallet,
+				Receiver:        "receiver@example.com",
+				Amount: &paypal.AmountPayout{
+					Value:    "9.99",
+					Currency: "USD",
+				},
+				Note:         "Thanks for your patronage!",
+				SenderItemID: fmt.Sprintf("%d",rand.Intn(100000000)),
+			},
+		},
+	}
+
+	payoutResp, err := c.CreatePayout(context.Background(), payout)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Payout Response:",payoutResp)
+}
--- go.mod
+++ go.mod
@@ -5,5 +5,8 @@ go 1.15
 require (
 	github.com/Shopify/sarama v1.27.2
 	github.com/gorilla/mux v1.8.0
+	github.com/joho/godotenv v1.3.0
 	github.com/mitchellh/mapstructure v1.4.1
+	//github.com/plutov/paypal v2.0.5+incompatible // indirect
+	github.com/plutov/paypal/v4 v4.0.0
 )
